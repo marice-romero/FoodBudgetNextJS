7f577cd028a656df159b2316eb345b2773922735
Marice Romero <marice.romero@gmail.com>
2024-11-12 13:14:31 -0800

    Added date ranges

--- src/utils/dateRanges.js
+++ src/utils/dateRanges.js
@@ -1,3 +1,8 @@
-export const dateRanges = ["8/11/2024 - 9/10/2024", "9/11/2024 - 10/10/2024"];
+export const dateRanges = [
+  "8/11/2024 - 9/10/2024",
+  "9/11/2024 - 9/30/2024",
+  "October 2024",
+  "November 2024",
+];
 
 export const currentDateRange = dateRanges[dateRanges.length - 1];
